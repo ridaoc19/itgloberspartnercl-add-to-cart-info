b055b21418f36edff341fc542de4daf43a88fd5a
ridaoc19 <ridaoc19@gmail.com>
2023-01-17 10:52:23 -0500

    feat: utilizacion de useOrderForm

--- react/components/AddToCartInfo.jsx
+++ react/components/AddToCartInfo.jsx
@@ -1,8 +1,35 @@
 import React from "react";
-import { useProduct } from 'vtex.product-context'
+import { useProduct } from 'vtex.product-context';
+import { useOrderForm } from 'vtex.order-manager/OrderForm'
+
 
 export default function AddToCartInfo() {
-    const productInfo = useProduct()
-    console.log("ejemplo",productInfo)
-    return( <div>maximiliano</div>)
+
+  const productInfo = useProduct() // "vtex.checkout-resources": "0.x",
+
+
+  const { orderForm: { items } } = useOrderForm() //   "vtex.order-manager": "0.x": pedido que esta en el carrito
+
+  // console.log("ejemplo", productInfo)
+
+  return (
+    <div>
+      {items?.map((item, index) => {
+        return (
+          <div key={index}>
+            <img src={item.imageUrls.at1x} alt="" />
+            <p>{item.name}</p>
+            <p>{item.id}</p>
+            <p>S/{item.price / 100}</p>
+            <p>Cant: {item.quantity}</p>
+          </div>
+        )
+      })}
+      <hr />
+      <div>
+        <p>Tienes {items.length} productos en el carrito</p>
+        {/* <p>Total ${total}</p> */}
+      </div>
+    </div>
+    )
 }
